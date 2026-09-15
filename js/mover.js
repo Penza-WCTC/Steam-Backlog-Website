30f66f1313932e8b676880a3d89c174f4aa0e3f2
Kyle Penza <kpenza1@my.wctc.edu>
2026-09-14 20:47:39 -0500

    1.2.1 - Finished Second Screen

--- js/mover.js
+++ js/mover.js
@@ -2,3 +2,8 @@ $(".move-on-1").on("click", function(){
     $("#view1").addClass("hidden");
     $("#view2").removeClass("hidden");
 })
+
+$(".move-on-2").on("click", function(){
+    $("#view2").addClass("hidden");
+    $("#view3").removeClass("hidden");
+})
