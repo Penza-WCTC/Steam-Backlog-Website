0c3422e8af305e5e1bb2bbd464e33314897eee68
Kyle Penza <kpenza1@my.wctc.edu>
2026-09-21 22:53:08 -0500

    1.6.2 - Review on Index load dynamically

--- js/app.js
+++ js/app.js
@@ -1,77 +1,61 @@
-import {createApp} from 'https://unpkg.com/vue@3/dist/vue.global.js';
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
 const app = createApp({
-    data:function(){
-        return{
-            newReview:{
-                userID,
-                gameID,
-                ratingNumber,
-                hours,
-                reasons,
-                finished,
-                review
+    data: function () {
+        return {
+            newReview: {
+                gameID : 0,
+                ratingNumber : 0,
+                hours : 0,
+                reasons : [],
+                finished : true,
+                review : "",
+                username : "",
+                communityVote: 0,
             },
-            newUser:{
-                userID,
-                image,
-                username
-            },
-            reviewList:{
-                [
-                    {
-                        userID = 1,
-                        gameID,
-                        ratingNumber = 1,
-                        hours = 0,
-                        reasons = ['Game got too repetitive','Game was boring'],
-                        finished = false,
-                        review = 'too many machines to build. would not build again. too many machines to build. would not build again. too many machines to build. would not build again.'
-                    },
+            reviewList: [
+                {
+                    userID: 1,
+                    gameID: 0,
+                    ratingNumber: 1,
+                    hours: 0,
+                    reasons: ['Game got too repetitive', 'Game was boring'],
+                    finished: false,
+                    review: 'too many machines to build. would not build again. too many machines to build. would not build again. too many machines to build. would not build again.',
+                    username: 'chromokiz',
+                    communityVote: 10,
+                },
 
-                    {
-                        userID = 2,
-                        gameID,
-                        ratingNumber = 4,
-                        hours = 40,
-                        reasons = ['Finished main quest'],
-                        finished = true,
-                        review = "I really like how you can build your own base. I think that it's neat that you can build any way you want. I wish the tutorial was more robust. I like it more then Satisfactory. Here are more test words to make this review longer then the last."
-                    }
-                ]
-            },
-            userList:{
-                [
-                    {
-                        userID = 1,
-                        image = 'chromokizProfileIcon',
-                        username = 'chromokiz'
-                    },
-                    {
-                        userID = 2,
-                        image = 'franksSinatraProfileIcon',
-                        username = 'Franks Sinatra'
-                    }
-                ]
-            }
-
+                {
+                    userID: 2,
+                    gameID : 0,
+                    ratingNumber: 4,
+                    hours: 40,
+                    reasons: ['Finished main quest'],
+                    finished: true,
+                    review: "I really like how you can build your own base. I think that it's neat that you can build any way you want. I wish the tutorial was more robust. I like it more then Satisfactory. Here are more test words to make this review longer then the last.",
+                    username: 'FranksSinatra',
+                    communityVote: 4,
+                },
+            ],
         }
     },
 
-    methods:{
+    methods: {
 
     },
 
-    computed:{
+    computed: {
 
     },
 
-    mounted:function(){
+    mounted: function () {
 
     },
-    
-    watch:{
+
+    watch: {
 
     },
 });
 
+export default app;
